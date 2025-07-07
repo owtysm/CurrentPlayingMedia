0bf9dd376fa4625b690cfc4e18a29dea492740e3
7ow <115172127+7owh@users.noreply.github.com>
2025-07-07 20:29:12 +0200

    pluh

--- CurrentPlayingMedia/CurrentPlayingMedia.cs
+++ CurrentPlayingMedia/CurrentPlayingMedia.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Threading.Tasks;
 using Windows.Media.Control;
+using Windows.Storage.Streams;
 
 namespace CurrentPlayingMedia
 {
@@ -9,86 +12,203 @@ namespace CurrentPlayingMedia
         public string Title { get; private set; }
         public string Artist { get; private set; }
         public string Album { get; private set; }
-        public GlobalSystemMediaTransportControlsSessionPlaybackStatus PlaybackStatus { get; private set; }
 
-        // so that the uninformed can just use `new PlayingMedia()`
-        // please dont kill me (please????)
-        public PlayingMedia()
+        public enum PlaybackStates
         {
-            var pmedia = Get().Result;
-            Title = pmedia.Title;
-            Artist = pmedia.Artist;
-            Album = pmedia.Album;
-            PlaybackStatus = pmedia.PlaybackStatus;
+            Changing,
+            Closed,
+            Opened,
+            Paused,
+            Playing,
+            Stopped
         }
 
+        private IRandomAccessStreamReference thumbnailRef;
 
-        // bullshit constructor for .Get() method
-        internal PlayingMedia(object _)
+        public event EventHandler<PlayingMedia> SongChanged;
+        public event EventHandler<PlaybackStates> PlaybackStateChanged;
+
+        public PlaybackStates PlaybackStatus { get; private set; }
+        private PlaybackStates? lastPlaybackStatus = null;
+
+        private GlobalSystemMediaTransportControlsSessionManager mediaManager;
+        private GlobalSystemMediaTransportControlsSession currentSession;
+
+        public PlayingMedia() { }
+
+        public async Task InitializeAsync()
         {
+            mediaManager = await GetMediaManagerAsync();
+            mediaManager.CurrentSessionChanged += OnCurrentSessionChanged;
 
+            await UpdateSessionAsync(mediaManager.GetCurrentSession());
         }
 
-        public static async Task<PlayingMedia> Get()
+        private async void OnCurrentSessionChanged(GlobalSystemMediaTransportControlsSessionManager sender, CurrentSessionChangedEventArgs args)
         {
-            var instance = new PlayingMedia(null);
+            await UpdateSessionAsync(sender.GetCurrentSession());
+        }
 
-            var mediaManager = await GetMediaManagerAsync();
-            var currentSession = mediaManager.GetCurrentSession();
-
-            if (currentSession == null)
+        private async Task UpdateSessionAsync(GlobalSystemMediaTransportControlsSession session)
+        {
+            if (currentSession != null)
             {
-                return null;
+                currentSession.MediaPropertiesChanged -= OnMediaPropertiesChanged;
+                currentSession.PlaybackInfoChanged -= OnMediaPropertiesChanged;
             }
 
-            var mediaProperties = await GetMediaPropertiesAsync(currentSession);
+            currentSession = session;
 
-            instance.Title = mediaProperties.Title;
-            instance.Artist = mediaProperties.Artist;
-            instance.Album = mediaProperties.AlbumTitle;
-            instance.PlaybackStatus = currentSession.GetPlaybackInfo().PlaybackStatus;
+            if (currentSession == null)
+                return;
 
-            return instance;
+            currentSession.MediaPropertiesChanged += OnMediaPropertiesChanged;
+            currentSession.PlaybackInfoChanged += OnMediaPropertiesChanged;
+
+            await UpdateMediaPropertiesAsync();
         }
 
-        private static Task<GlobalSystemMediaTransportControlsSessionManager> GetMediaManagerAsync()
+        private async void OnMediaPropertiesChanged(GlobalSystemMediaTransportControlsSession sender, object args)
+        {
+            await UpdateMediaPropertiesAsync();
+        }
+
+        private async Task UpdateMediaPropertiesAsync()
+        {
+            try
+            {
+                var mediaProperties = await GetMediaPropertiesAsync(currentSession);
+                Title = mediaProperties.Title;
+                Artist = mediaProperties.Artist;
+                Album = mediaProperties.AlbumTitle;
+
+                PlaybackStates newStatus = ParsePlaybackState(currentSession.GetPlaybackInfo().PlaybackStatus);
+                if (lastPlaybackStatus != newStatus)
+                {
+                    lastPlaybackStatus = newStatus;
+                    PlaybackStateChanged?.Invoke(this, newStatus);
+                }
+                PlaybackStatus = newStatus;
+
+                thumbnailRef = mediaProperties.Thumbnail;
+
+                SongChanged?.Invoke(this, this);
+            }
+            catch
+            {
+                // how did we get here
+            }
+        }
+
+        private PlaybackStates ParsePlaybackState(GlobalSystemMediaTransportControlsSessionPlaybackStatus playbackStatus)
+        {
+            switch (playbackStatus)
+            {
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Closed:
+                    return PlaybackStates.Closed;
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Opened:
+                    return PlaybackStates.Opened;
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Stopped:
+                    return PlaybackStates.Stopped;
+
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Paused:
+                    return PlaybackStates.Paused;
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing:
+                    return PlaybackStates.Playing;
+                case GlobalSystemMediaTransportControlsSessionPlaybackStatus.Changing:
+                    return PlaybackStates.Changing;
+                default:
+                    return PlaybackStates.Closed;
+            }
+        }
+
+        public async Task<Bitmap> GetThumbnailAsync()
+        {
+            if (thumbnailRef == null)
+                return null;
+
+            var stream = await OpenReadAsync(thumbnailRef);
+            var buffer = await ReadStreamToBufferAsync(stream);
+            return new Bitmap(new MemoryStream(buffer));
+        }
+
+        #region Wrapper Helpers
+
+        private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetMediaManagerAsync()
         {
             var tcs = new TaskCompletionSource<GlobalSystemMediaTransportControlsSessionManager>();
-            var operation = GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+            var op = GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
 
-            operation.Completed = (asyncOp, status) =>
+            op.Completed = (asyncOp, status) =>
             {
-                try
-                {
-                    tcs.SetResult(asyncOp.GetResults());
-                }
-                catch (Exception ex)
-                {
-                    tcs.SetException(ex);
-                }
+                try { tcs.SetResult(asyncOp.GetResults()); }
+                catch (Exception ex) { tcs.SetException(ex); }
             };
 
-            return tcs.Task;
+            return await tcs.Task;
         }
 
-        private static Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaPropertiesAsync(GlobalSystemMediaTransportControlsSession session)
+        private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaPropertiesAsync(GlobalSystemMediaTransportControlsSession session)
         {
             var tcs = new TaskCompletionSource<GlobalSystemMediaTransportControlsSessionMediaProperties>();
-            var operation = session.TryGetMediaPropertiesAsync();
+            var op = session.TryGetMediaPropertiesAsync();
 
-            operation.Completed = (asyncOp, status) =>
+            op.Completed = (asyncOp, status) =>
             {
-                try
-                {
-                    tcs.SetResult(asyncOp.GetResults());
-                }
-                catch (Exception ex)
-                {
-                    tcs.SetException(ex);
-                }
+                try { tcs.SetResult(asyncOp.GetResults()); }
+                catch (Exception ex) { tcs.SetException(ex); }
             };
+
+            return await tcs.Task;
+        }
+
+        private static async Task<IRandomAccessStreamWithContentType> OpenReadAsync(IRandomAccessStreamReference reference)
+        {
+            var tcs = new TaskCompletionSource<IRandomAccessStreamWithContentType>();
+            var op = reference.OpenReadAsync();
+
+            op.Completed = (asyncOp, status) =>
+            {
+                try { tcs.SetResult(asyncOp.GetResults()); }
+                catch (Exception ex) { tcs.SetException(ex); }
+            };
+
+            return await tcs.Task;
+        }
+
+        private static Task<byte[]> ReadStreamToBufferAsync(IRandomAccessStream stream)
+        {
+            var tcs = new TaskCompletionSource<byte[]>();
+
+            try
+            {
+                var reader = new DataReader(stream);
+                var size = (uint)stream.Size;
+                var loadOp = reader.LoadAsync(size);
+
+                loadOp.Completed = (op, status) =>
+                {
+                    try
+                    {
+                        op.GetResults();
+                        byte[] buffer = new byte[size];
+                        reader.ReadBytes(buffer);
+                        tcs.SetResult(buffer);
+                    }
+                    catch (Exception ex)
+                    {
+                        tcs.SetException(ex);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
 
             return tcs.Task;
         }
+
+        #endregion
     }
 }
